41a587903e554e73a5f303126e4d2264701fa137
charlesbaer <30347307+charlesbaer@users.noreply.github.com>
2023-02-02 14:26:31 -0500

    Create total_traffic_within_hour_by_destination_port.sql

---
+++ samples/vpc_flow_logs/total_traffic_within_hour_by_destination_port.sql
@@ -0,0 +1,44 @@
+/* Copyright 2023 Google LLC
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ https://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+ 
+CREATE TEMP FUNCTION PORTS_TO_PROTO(protocol INT64, src_port INT64, dst_port INT64) AS (
+CASE
+  WHEN protocol = 1 THEN 'icmp'
+  WHEN src_port = 22 OR dst_port = 22 THEN 'ssh'
+  WHEN src_port = 80 OR dst_port = 80 THEN 'http'
+  WHEN src_port = 8080 OR dst_port = 8080 THEN 'http'
+  WHEN src_port = 443 OR dst_port = 443 THEN 'https'
+  WHEN src_port = 53 OR dst_port = 53 THEN 'dns'
+  WHEN src_port = 10402 OR dst_port = 10402 THEN 'gae' -- App Engine flexible environment
+  WHEN src_port = 8443 OR dst_port = 8443 THEN 'gae' -- App Engine flexible environment
+  ELSE FORMAT('other-%d->%d', src_port, dst_port)
+END
+);
+
+
+SELECT
+ PORTS_TO_PROTO(
+   CAST(JSON_VALUE(json_payload.connection.protocol) as INT64),
+   CAST(JSON_VALUE(json_payload.connection.src_port) as INT64),
+   CAST(JSON_VALUE(json_payload.connection.dest_port) as INT64)) as port,
+ SUM(CAST(JSON_VALUE(json_payload.bytes_sent) as INT64)) as total_bytes_sent
+FROM
+ `[MY_PROJECT].global._Default._AllLogs`
+WHERE
+ log_name LIKE "%compute.googleapis.com%2Fvpc_flows%"
+ AND timestamp(replace(substr(JSON_VALUE(json_payload.start_time),0,19),"T"," ")) > TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 1 HOUR)
+ AND JSON_VALUE(json_payload.reporter) = "SRC"
+GROUP BY 1
+ORDER BY 2 DESC
