f9a377d1c5157c8de70474da7c272e1f46e42dd1
charlesbaer <30347307+charlesbaer@users.noreply.github.com>
2023-04-24 17:11:25 -0400

    Update total_traffic_within_hour_by_destination_port.sql

--- samples/vpc_flow_logs/total_traffic_within_hour_by_destination_port.sql
+++ samples/vpc_flow_logs/total_traffic_within_hour_by_destination_port.sql
@@ -39,6 +39,6 @@ FROM
 WHERE
  log_id = "compute.googleapis.com/vpc_flows"
  AND timestamp(replace(substr(JSON_VALUE(json_payload.start_time),0,19),"T"," ")) > TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 1 HOUR)
- AND JSON_VALUE(json_payload.reporter) = "SRC"
+ AND JSON_VALUE(json_payload.reporter) = "SRC")
 GROUP BY 1
 ORDER BY 2 DESC
